ec3ff36390657b0be43181f7c5f242aac208508e
gliadev <gliatem@gmail.com>
2023-12-12 09:36:01 +0100

    Initial Commit

---
+++ AppPruebaGit/AppPruebaGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppPruebaGitApp.swift
+//  AppPruebaGit
+//
+//  Created by Adolfo on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AppPruebaGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppPruebaGit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppPruebaGit
+//
+//  Created by Adolfo on 12/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
